d80b8fa496a45350e361fa839fd2512855c34920
Cunvoker <swingxzace@gmail.com>
2018-07-31 22:03:16 +0800

    Machine class

---
+++ Machine.java
@@ -0,0 +1,27 @@
+public class Machine{
+    private int id;
+    private int cpu;
+    private int mem;
+    private int disk;
+
+    private int pLimit;
+    private int mLimit;
+    private int pmLimit;
+
+
+    protected void create(int i, int c, int m, int d, int pl, int ml, int pml){
+        id       = i;
+        cpu      = c;
+        mem      = m;
+        disk     = d;
+        pLimit   = pl;
+        mLimit   = ml;
+        pmLimit  = pml;
+    }
+
+    public static void main(String[] args){
+        Machine ma = new Machine();
+        ma.create(1,2,3,4,5,6,7);
+        System.out.println(ma.id);
+    }
+}
